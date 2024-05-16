713120ccde07c419a22658e4458642f48d89e245
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-05-16 22:07:07 +0100

    almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import './index.css'
+import Table from './Table'
+
+import {Users} from './users'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [query, setQuery] = useState("")
+  // console.log(Users.filter(user=>user.first_name.toLowerCase().includes(query)));
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <div className="app flex  item-center justify-center ml-[20rem]">
+      <input type="text" placeholder='Search' className='search' name="" id=""
+      value={query}
+      onChange={(e)=>{setQuery(e.target.value)}}
+      />
+
+      <Table
+      data={Users}
+      query={query}
+      
+      />
+
+
+      {/* <ul className="list">
+        {Users.filter(user=>user.first_name.toLowerCase().includes(query)).map((user)=>(
+                  <li className="listItem">{user.first_name}</li>
+
+        ))}
+      
+      </ul> */}
+     </div>
     </>
   )
 }
